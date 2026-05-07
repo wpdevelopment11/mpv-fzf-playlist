1fc1f60e3bb96791e6a3324de98976c7d76ab8da
wpdevelopment11 <wpdevelopment11@gmail.com>
2026-05-07 14:14:06 +0300

    Initial version

---
+++ pl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RELOAD='
+playlist=$(socat - /tmp/mpvsocket <<< "{\"command\": [\"get_property\", \"playlist\"]}")
+err=$(jq -r .error <<< $playlist)
+if [[ $err != "success" ]]; then
+    [[ "$err" ]] && echo "Error: $err"
+    exit 1
+fi
+jq -r ".data[] | (if .title then \"\(.title)\t\" else \"\t\" end) + .filename" <<< $playlist | column -t -s '"\$'\t'"
+
+fzf --layout=reverse-list \
+    --bind "start,ctrl-r:reload:$RELOAD" \
+    --bind 'enter:execute-silent(socat - /tmp/mpvsocket <<< "{ \"command\": [\"playlist-play-index\", "{n}"] }" > /dev/null)'
+
