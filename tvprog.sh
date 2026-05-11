dcf94929741f5bbb822fe72a1947888b4720236f
wpdevelopment11 <wpdevelopment11@gmail.com>
2026-05-11 18:37:24 +0300

    Show a table of TV programs for the channels passed as CLI arguments

--- tvprog.sh
+++ tvprog.sh
@@ -3,12 +3,8 @@
 set -u
 
 # xq-python --xml-force-list 'display-name' . epg.xml > epg.json
-epg=epg_one.json
+epg=epg.json
 
-chanid=$(jq -r --arg channel "$1" 'first(.tv.channel[] | select(.["display-name"][] | [if type == "object" then .["#text"] end] | any(. == $channel)) | .["@id"])' "$epg")
-if [[ ! $chanid ]]; then
-    echo "Channel '$1' is not found!" >&2
-    exit 1
-fi
+channels=$(jq --argjson channels "$(printf "%s\n" "$@" | jq -R | jq -s)" '[$channels[] as $channel | first(.tv.channel[] | select(.["display-name"][] | [if type == "object" then .["#text"] end] | any(. == $channel)) | {name: $channel, id: .["@id"]})]' "$epg")
 
-jq --arg chanid "$chanid" '[.tv.programme[] | select(.["@channel"] == $chanid and .["@stop"] >= (now | strflocaltime("%Y%m%d%H%M%S %z")))][:2]' "$epg"
+jq -r --argjson channels "$channels" '$channels[] as $ch | [$ch.name, (.tv.programme[] | select(.["@channel"] == $ch.id and .["@stop"] >= (now | strflocaltime("%Y%m%d%H%M%S %z"))) | .title | if type == "object" then .["#text"] end)][:3] | @tsv' "$epg" | column -t -s $'\t'
