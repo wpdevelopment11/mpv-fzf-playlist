341788b6f38e7611a871762f8558b7f281f2b47f
wpdevelopment11 <wpdevelopment11@gmail.com>
2026-05-12 22:24:35 +0300

    Show the start time of the next TV program

--- tvprog.sh
+++ tvprog.sh
@@ -15,7 +15,6 @@ jq -nr \
 | {name: $channel, id: .["@id"]})
 | . as $ch
 | [$ch.name, ($tv.programme[]
-| select(.["@channel"] == $ch.id and .["@stop"] >= (now | strflocaltime("%Y%m%d%H%M%S %z")))
-| .title
-| if type == "object" then .["#text"] end)][:3]
-| @tsv' "$epg" <(printf "%s\n" "$@" | jq -R | jq -s) | column -t -s $'\t'
+    | select(.["@channel"] == $ch.id and .["@stop"] >= (now | strflocaltime("%Y%m%d%H%M%S %z")))
+    | [(.title | if type == "object" then .["#text"] end), (.["@start"] | strptime("%Y%m%d%H%M%S %z") | mktime | strftime("%H:%M"))])][:3]
+| .[1][1] |= empty | flatten | @tsv' "$epg" <(printf "%s\n" "$@" | jq -R | jq -s)
