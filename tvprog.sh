0ee096cb49f2fba72376360ce5634ea6f8865f38
wpdevelopment11 <wpdevelopment11@gmail.com>
2026-05-12 11:45:36 +0300

    Reformat the jq program to make it more readable

--- tvprog.sh
+++ tvprog.sh
@@ -5,6 +5,17 @@ set -u
 # xq-python --xml-force-list 'display-name' . epg.xml > epg.json
 epg=epg.json
 
-channels=$(jq --argjson channels "$(printf "%s\n" "$@" | jq -R | jq -s)" '[$channels[] as $channel | first(.tv.channel[] | select(.["display-name"][] | [if type == "object" then .["#text"] end] | any(. == $channel)) | {name: $channel, id: .["@id"]})]' "$epg")
-
-jq -r --argjson channels "$channels" '$channels[] as $ch | [$ch.name, (.tv.programme[] | select(.["@channel"] == $ch.id and .["@stop"] >= (now | strflocaltime("%Y%m%d%H%M%S %z"))) | .title | if type == "object" then .["#text"] end)][:3] | @tsv' "$epg" | column -t -s $'\t'
+jq -nr \
+ '(input | .tv) as $tv
+| (input | .[]) as $channel
+| first($tv.channel[]
+| select(.["display-name"][]
+    | [if type == "object" then .["#text"] end]
+    | any(. == $channel))
+| {name: $channel, id: .["@id"]})
+| . as $ch
+| [$ch.name, ($tv.programme[]
+| select(.["@channel"] == $ch.id and .["@stop"] >= (now | strflocaltime("%Y%m%d%H%M%S %z")))
+| .title
+| if type == "object" then .["#text"] end)][:3]
+| @tsv' "$epg" <(printf "%s\n" "$@" | jq -R | jq -s) | column -t -s $'\t'
