bde861eda57f946e9bb1a3bdb3872e12ec5a3c4e
wpdevelopment11 <wpdevelopment11@gmail.com>
2026-05-14 18:39:13 +0300

    Merge everything into a single file
    To make it easy to install and use

--- mpvpl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RELOAD='join -a 1 -1 2 -t $'"'\t'"' <(./playlist.sh) <(./tv.sh) | awk -F"\t" '"'"'BEGIN{OFS="\t"} {tmp=$1; $1=$2; $2=tmp; print}'"'"' | column -t -s $'"'\t'"
-
-fzf --layout=reverse-list \
-    --bind "start,ctrl-r:reload:$RELOAD" \
-    --bind 'enter:execute-silent(socat - /tmp/mpvsocket <<< "{ \"command\": [\"playlist-play-index\", "{1}"] }" > /dev/null)'
-
--- playlist.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-socat - /tmp/mpvsocket <<< '{"command": ["get_property", "playlist"]}' \
-    | jq -r ".data | to_entries[] |  [.key, .value.title, .value.filename] | map(values) | @tsv" | sort -bk2,2 -t $'\t'
